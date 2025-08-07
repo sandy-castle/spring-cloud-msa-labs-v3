1c1f95999e1fbbd25b9e8bede62b0c40bf323232
mz\user <jslovejs182@gmail.com>
2025-08-07 12:27:25 +0900

    feat: DB용  컴포즈 및 초기화 스크립트 작성

---
+++ backend/infra/mysql/init/01-create-databases.sql
@@ -0,0 +1,8 @@
+
+DROP DATABASE IF EXISTS `user_service_db`;
+DROP DATABASE IF EXISTS `product_service_db`;
+DROP DATABASE IF EXISTS `order_service_db`;
+
+CREATE DATABASE `user_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `product_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `order_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
